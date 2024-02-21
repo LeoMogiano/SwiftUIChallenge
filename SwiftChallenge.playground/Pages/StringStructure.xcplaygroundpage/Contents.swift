afe401de0bb0ec25fdd197d02a70289cbd44e9f1
Leo Mogiano <leomogiano@outlook.com>
2024-02-20 22:54:56 -0400

    Testing string structure

--- SwiftChallenge.playground/Pages/StringStructure.xcplaygroundpage/Contents.swift
+++ SwiftChallenge.playground/Pages/StringStructure.xcplaygroundpage/Contents.swift
@@ -1,1 +1,22 @@
+import Foundation
+
+let name = "Leo Mogiano"
+let file = "photo.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Hola Xd \"Mogi\" Xd."
+
+let movie = """
+A day in
+the life of an
+Apple engineer 
+"""
+let nameLength = name.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(file.hasSuffix(".jpg"))
+
 
