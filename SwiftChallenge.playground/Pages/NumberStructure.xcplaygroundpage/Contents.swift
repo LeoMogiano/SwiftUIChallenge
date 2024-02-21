66c254757526e9c592cdba25cb93a2e73380fc7a
Leo Mogiano <leomogiano@outlook.com>
2024-02-20 23:02:57 -0400

    Checking how to store numbers

---
+++ SwiftChallenge.playground/Pages/NumberStructure.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let score = 10
+let reallyBig = 1_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubleScore = score * 2
+let squaredScore = score * score
+let helvedScore = score / 2
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter -= 5
+counter *= 5
+counter /= 2
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(reallyBig)
