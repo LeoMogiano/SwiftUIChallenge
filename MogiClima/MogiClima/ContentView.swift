6ec02c9ba4c6e9f91a2755e5bb4bb71da7d692d7
Leo Mogiano <leomogiano@outlook.com>
2024-03-11 01:33:14 -0400

    Uploaded for day nineteen

---
+++ MogiClima/MogiClima/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MogiClima
+//
+//  Created by Leo Mogiano on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MogiClima/MogiClima/MogiClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MogiClimaApp.swift
+//  MogiClima
+//
+//  Created by Leo Mogiano on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MogiClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
