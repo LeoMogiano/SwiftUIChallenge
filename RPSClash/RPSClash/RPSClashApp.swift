295f1466cd64dcefe4656d1aa9abfae15e9bd256
Leo Mogiano <leomogiano@outlook.com>
2024-03-27 23:58:00 -0400

    Uploaded for day twenty five :)

---
+++ RPSClash/RPSClash/RPSClashApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSClashApp.swift
+//  RPSClash
+//
+//  Created by Leo Mogiano on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RPSClashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
