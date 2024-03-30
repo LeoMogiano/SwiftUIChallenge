94664c078feb3d7f18d353485fdcf98c012a160b
Leo Mogiano <leomogiano@outlook.com>
2024-03-30 00:36:59 -0400

    Uploaded for day twenty five :)

--- RPSClash/RPSClash/ContentView.swift
+++ RPSClash/RPSClash/ContentView.swift
@@ -9,45 +9,79 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let selectionRPS = ["Rock", "Paper","Scissors"].shuffled()
-    let correctsRPS = ["Paper", "Rock", "Scissors"]
+    @State private var selectionRPS = ["🪨", "📄","✂️"].shuffled()
+//    let correctsRPS = ["Paper", "Rock", "Scissors"]
     
     @State private var selectionApp = Int.random(in: 0...2)
+    @State private var youWon = false
     @State private var completeGame = false
     @State private var showingScore = false
     @State private var scoreResult = ""
     @State private var score = 0
     @State private var phase = 0
     
+    @State private var yOffset: Double = 0
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.green, .black], startPoint: .top, endPoint: .bottom)
+            LinearGradient(colors: [.gray, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack {
                 Spacer()
-                    .frame(height: 20)
+                    .frame(height: 25)
                 Text("Rock, Paper and Scissors")
                     .foregroundStyle(.white)
-                    .font(.largeTitle)
+                    .font(.system(size: 25).bold())
                 Spacer()
                     .frame(height: 20)
-                Text("Esta es la fase \(phase)")
+                Text("Phase \(phase)")
                     .foregroundStyle(.white)
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                Spacer().frame(height: 160)
+                Spacer().frame(height: 50)
                 
+                (!showingScore)
+                ? AnyView(Text("💬")
+                    .font(.system(size: 60))
+                    .offset(y: yOffset)
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 1).repeatForever()) {
+                            self.yOffset = 15 // Ajusta la amplitud del movimiento aquí
+                        }
+                    })
+                : AnyView(Text(selectionRPS[selectionApp])
+                    .font(.system(size: 60))
+                )
                 
+
+                Spacer().frame(height: 50)
                 
-                VStack(spacing:30) {
-                    Text("Select your choice")
-                        .foregroundStyle(.white)
-                    ForEach(selectionRPS, id: \.self) {
-                        Text("\($0) 🪨")
-                            .foregroundStyle(.white)
+                Text("Select your choice")
+                    .foregroundStyle(.white)
+                    .font(.title3.bold())
+                Spacer().frame(height: 30)
+                HStack(spacing:30) {
+                    
+                    ForEach(selectionRPS, id: \.self) { emoji in
+                        Button {
+                            rpsTapped(selection: emoji)
+                        } label: {
+                            Text(emoji)
+                                .font(.system(size: 60))
+                        }
+                        .alert("Now you have,\(score) points", isPresented: $showingScore) {
+                            Button("Continue", action: nextMatch)
+                        } message: {
+                            Text(scoreResult)
+                                .font(.title2)
+                                .foregroundStyle(youWon ? .green : .red)
+                        }
                     }
                 }
+                Spacer().frame(height: 100)
+                Text( (showingScore) ? "Your score is \(score)" : "")
+                    .foregroundStyle(youWon ? .green : .red)
+                    
                 Spacer()
                 
             }
@@ -55,13 +89,32 @@ struct ContentView: View {
         
     }
     
-    func rpsTapped(number: Int) {
-        let indexApp = correctsRPS.firstIndex(of: correctsRPS[selectionApp]) ?? 0
-        let indexUser = correctsRPS.firstIndex(of: correctsRPS[number]) ??  0
-        if ( indexUser == indexApp + 2 ) {
-            scoreResult = "Lo sentimos has perdido!"
-            showingScore = true
+    func rpsTapped(selection: String) {
+        let appSelection = selectionRPS[selectionApp]
+        print(selection)
+        print(appSelection)
+        
+        if appSelection == selection {
+            scoreResult = "😱! It's a Draw"
+        } else if (appSelection == "🪨" && selection == "📄") ||
+                  (appSelection == "📄" && selection == "✂️") ||
+                  (appSelection == "✂️" && selection == "🪨") {
+            score += 5
+            scoreResult = "🤩 You are doing great!"
+            youWon = true
+        } else {
+            score -= 5
+            scoreResult = "🙁 You lost"
+            youWon = false
         }
+        
+        showingScore = true
+    }
+    
+    func nextMatch () {
+        selectionRPS = selectionRPS.shuffled()
+        selectionApp = Int.random(in: 0...2)
+        yOffset = 0
     }
 }
 
