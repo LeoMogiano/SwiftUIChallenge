a0e3b6a05c4fd8f2ea560db860fcd0809b1de45b
Leo Mogiano <leomogiano@outlook.com>
2024-04-08 00:32:37 -0400

    Uploaded for day twenty seven :)

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
     
+    @State private var alerTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
  
     var body: some View {
         //        VStack {
@@ -41,9 +45,42 @@ struct ContentView: View {
                     .padding(.horizontal, 100)
                 Spacer()
                 
-            }.navigationTitle("BetterRest Quiz")
+            }
+            .navigationTitle("BetterRest Quiz")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+            .alert(alerTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
+
         }
     
+    }
+    
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minutes = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Int64((hour + minutes)), estimatedSleep: sleepAmount, coffee: Int64(Double(coffeeAmount)))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alerTitle = "Your ideal bed time is..."
+            alertMessage = "\(sleepTime.formatted(date:.omitted, time: .shortened))"
+        } catch {
+            alerTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bed time."
+        }
+        
+        showingAlert = true
     }
     
 //    func exampleDates() {
