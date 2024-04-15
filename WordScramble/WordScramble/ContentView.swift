ffae9619aa4ddc184f5ef0e53b70077fb7c079a3
Leo Mogiano <leomogiano@outlook.com>
2024-04-15 01:40:57 -0400

    Uploaded for day thirty :)

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,22 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    var names = ["Leo", "Mogiano", "Gutierrez"]
+    //    var names = ["Leo", "Mogiano", "Gutierrez"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        List {
-            Section {
-                ForEach(names, id: \.self) {
-                    Text("\($0)")
+        
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text("\(word)")
+                        }
+                    }
                 }
             }
-            Section {
-                ForEach(0..<5) {
-                    Text("\($0)")
-                }
-            }
-            
+            .navigationTitle(rootWord)
+            .onSubmit(addWord)
+            .onAppear(perform: startGame)
         }
-       
+        
+    }
+    
+    func addWord() {
+        let answer =  newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else { return }
+        
+        // extra validation
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        
+        newWord = ""
+        
+    }
+    
+    func startGame() {
+        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordURL) {
+                let allWord = startWords.components(separatedBy: "/n")
+                rootWord = allWord.randomElement() ?? "silkorm"
+            }
+        }
+        
+        fatalError("Could not load start.txt from the bundle.")
     }
 }
 
