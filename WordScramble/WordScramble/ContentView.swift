e32793b47b669a6a79e3bd9b6586c01dfabe52c6
Leo Mogiano <leomogiano@outlook.com>
2024-04-16 01:35:12 -0400

    Uploaded for day thirthy :)

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -13,6 +13,10 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
         
         NavigationStack {
@@ -35,6 +39,9 @@ struct ContentView: View {
             .navigationTitle(rootWord)
             .onSubmit(addWord)
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) { } message: {
+                Text(errorMessage)
+            }
         }
         
     }
@@ -44,7 +51,21 @@ struct ContentView: View {
         
         guard answer.count > 0 else { return }
         
-        // extra validation
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell the word from \(rootWord)")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
@@ -54,14 +75,56 @@ struct ContentView: View {
     }
     
     func startGame() {
-        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            if let startWords = try? String(contentsOf: startWordURL) {
-                let allWord = startWords.components(separatedBy: "/n")
-                rootWord = allWord.randomElement() ?? "silkorm"
-            }
+        // 1.find the URL for start.txt in our app bundle
+        guard let startWordsURL = Bundle.main.url(forResource: "start",withExtension: "txt") else{
+            //assign the default value
+            rootWord = "yamori"
+            // if the url is not found this will print in the console
+            print("File could not be found")
+            return
         }
         
-        fatalError("Could not load start.txt from the bundle.")
+        do{
+            // 2.load start.txt into a string
+            let startWords = try String(contentsOf: startWordsURL)
+            // 3.split the string up into an array of strings, splitting on line breaks
+            let allWords = startWords.components(separatedBy: "\n")
+            // 4.pick one random word, or use "yamori" as a sensible default
+            rootWord = allWords.randomElement() ?? "yamori"
+        } catch{
+            //if the file couldn´t be read this will print the error in the console
+            print(error)
+        }
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
