1553c1a9ec98ce430b1c39d8628502bfcf24b428
Leo Mogiano <leomogiano@outlook.com>
2024-04-23 00:32:37 -0400

    Uploaded for day thirty two :)

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,36 +9,83 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
         VStack (alignment:.center) {
-            Text("Hola")
-                .padding(50)
-                .background(.red)
-                .foregroundStyle(.white)
-                .scaleEffect(animationAmount)
-                .blur(radius: 1.0)
-                .animation(.default,value: animationAmount)
-            Spacer()
-                .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-            HStack {
-                Button("Tap Me") {
-                    animationAmount += 0.5
+//            Text("Hola")
+//                .padding(50)
+//                .background(.red)
+//                .foregroundStyle(.white)
+//                .scaleEffect(animationAmount)
+//                .blur(radius: 0.0)
+//                .animation(
+//                    .easeInOut(duration: 2)
+//                    .repeatCount(2, autoreverses: true)
+//                           ,value: animationAmount
+//                )
+//                
+//            Spacer()
+//                .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+//            HStack {
+//                Button("Tap Me") {
+//                    animationAmount += 0.5
+//                }
+//                
+//                .padding(20)
+//                .background(.red)
+//                .clipShape(.circle)
+//                .foregroundStyle(.white)
+//                Button("Tap Me") {
+//                    animationAmount -= 0.5
+//                }
+//                .padding(20)
+//                .background(.blue)
+//                .foregroundStyle(.white)
+//                .clipShape(.circle)
+//            }
+            
+//            Button("Tap me") {
+////                animationAmount += 1
+//            }
+//            .padding(50)
+//            .background(.red)
+//            .foregroundStyle(.white)
+//            .clipShape(.circle)
+//            .overlay(
+//                Circle()
+//                    .stroke(.red)
+//                    .scaleEffect(animationAmount)
+//                    .opacity(2 - animationAmount)
+//                    .animation(.easeOut(duration: 1).repeatForever(autoreverses: false)
+//                               ,value: animationAmount)
+//            ).onAppear{
+//                animationAmount = 2
+//            }
+            
+//            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+//
+//            Spacer()
+//
+//            Button("Tap Me") {
+//                animationAmount += 1
+//            }
+//            .padding(40)
+//            .background(.red)
+//            .foregroundStyle(.white)
+//            .clipShape(.circle)
+//            .scaleEffect(animationAmount)
+            
+            Button("Tap Me") {
+                withAnimation (.spring(duration: 1, bounce: 0.5)) {
+                    animationAmount += 360
                 }
-                
-                .padding(20)
-                .background(.red)
-                .clipShape(.circle)
-                .foregroundStyle(.white)
-                Button("Tap Me") {
-                    animationAmount -= 0.5
-                }
-                .padding(20)
-                .background(.blue)
-                .foregroundStyle(.white)
-                .clipShape(.circle)
             }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
         }
     }
 }
