8fc4d1039267775aa6c4d36ea08f3ce74c433b53
Leo Mogiano <leomogiano@outlook.com>
2024-04-30 01:06:18 -0400

    Uploaded for thirty six day :)

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Leo Mogiano on 30/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Observable
+    class User {
+        var firstName = "Leo"
+        var lastName = "Mogiano"
+    }
+    
+    @State private var mogiUser = User()
+    
+    var body: some View {
+        VStack {
+            Text("\(mogiUser.firstName)")
+            Text("\(mogiUser.lastName)")
+            
+            TextField("Input de Nombre", text: $mogiUser.firstName)
+            TextField("Input de Nombre", text: $mogiUser.lastName)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Leo Mogiano on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
