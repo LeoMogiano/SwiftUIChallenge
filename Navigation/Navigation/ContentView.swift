67429b9a7c31938acbcbc0d6276496f8bfe56380
Leo Mogiano <leomogiano@outlook.com>
2024-12-06 00:34:46 -0400

    Day 44: Handling navigation ex. with persistent state.

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -6,85 +6,134 @@
 //
 
 import SwiftUI
+import Observation
+
+
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+
+        // Still here? Start with an empty path.
+        path = NavigationPath()
+    }
+
+    func save() {
+        guard let representation = path.codable else { return }
+
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
 
 struct DetailView: View {
     var number: Int
 
     var body: some View {
-        Text("Detail View \(number)")
-    }
-
-    init(number: Int) {
-        self.number = number
-        print("Creating detail view \(number)")
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
     }
 }
 
 struct ContentView: View {
-    @State private var path: [String] = []
-    
+    @State private var pathStore = PathStore()
+
     var body: some View {
-        
-//            Problem with simple navigation
-//            Load views before handling
-//            List(0..<1000) { i in
-//                NavigationLink("Tap Me") {
-//                    DetailView(number: i)
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i)
+                }
+        }
+    }
+}
+
+//struct ContentView: View {
+//    @State private var path = [String]()
+//    let user: String = "Leo Mogiano"
+//    
+//    var body: some View {
+//        
+////            Problem with simple navigation
+////            Load views before handling
+////            List(0..<1000) { i in
+////                NavigationLink("Tap Me") {
+////                    DetailView(number: i)
+////                }
+////            }
+//        
+//        NavigationStack(path: $path) {
+//            VStack(spacing: 20) {
+//                Text("Pantalla Principal")
+//                    .font(.largeTitle)
+//                    .bold()
+//                
+//                Button("Ir a Perfil") {
+//                    path.append("profile")
+//                }
+//                
+//                Button("Ir a Configuración") {
+//                    path.append("settings")
 //                }
 //            }
-        
-        NavigationStack(path: $path) {
-            VStack(spacing: 20) {
-                Text("Pantalla Principal")
-                    .font(.largeTitle)
-                    .bold()
-                
-                Button("Ir a Perfil") {
-                    path.append("profile")
-                }
-                
-                Button("Ir a Configuración") {
-                    path.append("settings")
-                }
-            }
-            .padding()
-            .navigationDestination(for: String.self) { route in
-                switch route {
-                case "profile":
-                    ProfileView()
-                case "settings":
-                    SettingsView()
-                default:
-                    Text("Ruta desconocida")
-                }
-            }
-        }
-    }
-}
-
-struct ProfileView: View {
-    var body: some View {
-        VStack(spacing: 20) {
-            Text("Perfil de Usuario")
-                .font(.title)
-                .bold()
-            Text("Aquí puedes ver y editar tu información personal.")
-        }
-        .padding()
-    }
-}
-
-struct SettingsView: View {
-    var body: some View {
-        VStack(spacing: 20) {
-            Text("Configuración")
-                .font(.title)
-                .bold()
-            Text("Ajusta las preferencias de la aplicación aquí.")
-        }
-        .padding()
-    }
-}
+//            .padding()
+//            .navigationDestination(for: String.self) { route in
+//                switch route {
+//                case "profile":
+//                    ProfileView(userName: user)
+//                case "settings":
+//                    SettingsView()
+//                default:
+//                    Text("Ruta desconocida")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//struct ProfileView: View {
+//    
+//    let userName:String
+//    
+//    var body: some View {
+//        VStack(spacing: 20) {
+//            Text("Perfil de Usuario: \(userName)")
+//                .font(.title)
+//                .bold()
+//            Text("Aquí puedes ver y editar tu información personal.")
+//        }
+//        .padding()
+//    }
+//}
+//
+//struct SettingsView: View {
+//    var body: some View {
+//        VStack(spacing: 20) {
+//            Text("Configuración")
+//                .font(.title)
+//                .bold()
+//            Text("Ajusta las preferencias de la aplicación aquí.")
+//        }
+//        .padding()
+//    }
+//}
 
 
 #Preview {
