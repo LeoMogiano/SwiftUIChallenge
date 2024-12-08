c7dc740a465f06b85175f3e040d230e90703874a
Leo Mogiano <leomogiano@outlook.com>
2024-12-07 23:51:27 -0400

    Day 49: New project CupcakeCorner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Leo Mogiano on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
